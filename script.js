b4c7274ec33a9b4371eb9d0d7d85432637e805dd
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2021-11-04 18:24:53 +0000

    Added solutions

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ var names = ["Umkulthom", "Roland", "Lisa", "Moussa", "Mamadou", "Nana Ama", "An
 // 1. Add 5 of your favorite songs to the list. Use a for loop to print out each item by saying "I like to listen to Halo", "I like to listen to Crazy in Love", etc.
 var bestBeyonceSongs = ["Crazy in Love", "Love on Top", "Freedom", "Halo", "Get Me Bodied"]
 
-
+for(var i = 0; i < bestBeyonceSongs.length; i++){
+	console.log("I like to listen to " + bestBeyonceSongs[i])
+}
 
 
 
 
 // 2. BRING IT TOGETHER: I have given you a large list of numbers with scores from a recent project. I have made a typo in everyone's grade and they are too large. Divide each grade my 100 to get the actual grade. Print out the correct value to the console.
 var grades = [1891,1975,1987,1923,1928,1993,1946,1947,2005,1897,1971,1929,1875,1945,1680,811,1901,1396,1942,1282,1941,1978,1884,1879,1230,2010,1881,1979,1996,1904,1934,1865,2003,2006,1966,1860,1259,1959,1931,1963,1878,1880,151,1925,1663,1908,1863,1391,1922,1968,1998,1084,1982,1960,1938,1876,1937,1882,1873,1926,1986,1416,1864,1862,1969,1913,532,1866,1242,1933,1903,965,1927,1890,1991,1388,1992,1902,1907,1964,1394,2009,1920,630,1932,1854,1951,1852,1983,1314,1855,1954,1921,1989,1871,1995,1885,1974,1915,1872,1251,1899,1985,1889,1935,1912,946,1965,1739,1973,1911,1910,1917,1918,1900,1886,1477,2000,1916,1077,2004,1456,1867,1970,1999,1919,1726,706,1930,1994,1988,1997,1870,1953,652,1893,1898,1883,1957,1972,1874,1977,1955,2001,1906,1389,1848,1940,1877,1962,1948,1887,1924,1403,1408,1861,1892,1990,1222,677,1392,1113,1085,1894,1106,1939,1961,1944,1952,1643,1404,1895,1958,1976,1206,1905,1076,1888,1896,1943,1950,2008,1967,164,1981,1868,1914,1909,1956,341,1379,2007,1563,1980,1072,1949,1250,1258,1092,2002];
-
+for(var i = 0; i < grades.length; i++){
+	console.log(grades[i]/10)
+}
 
 
 
@@ -30,7 +34,51 @@ function setup() {
 }
 
 function draw() {
+	// Question 3
+	// for(var i = 0; i < colors.length; i++){
+	// 	fill(colors[i])
+	// 	rect(50*i, 50, 50, 50)
+	// }
+
+	// Question 4
+	// for(var i = 0; i < colors.length; i++){
+	// 	fill(colors[i])
+	// 	ellipse(50, 40*i, 50, 50)
+	// }
 	
+	// Question 5
+	// for(var i = 0; i < sizes.length; i++){
+	// 	ellipse(50 * i, 100, sizes[i], sizes[i])
+	// }
+	
+	// Quesiton 6
+	// sizes.sort()
+	// for(var i = 0; i < sizes.length; i++){
+	// 	fill(colors[i])
+	// 	ellipse(100, 50 * i, sizes[i], sizes[i])
+	// }
+
+	//Question 7
+	// sizes.sort()
+	// var position = sizes[0] / 2;
+  // for(i = 0; i < colors.length; i++){
+  //   fill(colors[i]);
+  //   ellipse(width / 2, position, sizes[i])
+  // 	position = position + sizes[i]
+  // }
+
+	// Question 8
+	// counter = 0
+	// for(let x = 0; x < width; x+=50){
+	// 	for(let y = 0; y < height; y+=50){
+	// 		if(counter >= colors.length){
+	// 			counter = 0
+	// 		}
+	// 		fill(colors[counter])
+	// 		rect(x, y, 50, 50)
+	// 		counter++
+	// 	}
+	// }
 	
 	
 }
